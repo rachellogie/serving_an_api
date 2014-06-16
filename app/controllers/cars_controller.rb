8286b039129fec6795c3a6dc1ee0e5829882bd39
rachellogie <rachel.logie1@gmail.com>
2014-06-16 14:55:53 -0600

    you can get a specific car and if it doesn't
    exist then you will get back a 404

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,4 +3,13 @@ class CarsController < ActionController::Base
   def index
     @cars = Car.all
   end
+
+  def show
+    if Car.exists?(params[:id])
+      @car = Car.find(params[:id])
+      render status: 200
+    else
+      render json: {}, status: 404
+    end
+  end
 end
--- spec/api/car_spec.rb
+++ spec/api/car_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 describe "Make Cars API" do
 
+  before do
+
+  end
+
   describe "GET /cars" do
     it "returns a list of cars" do
 
-      subaru = create_make(name: "Subaru")
-      toyota = create_make(name: "Toyota")
+      subaru = create_make(:name => "Subaru")
+      toyota = create_make(:name => "Toyota")
 
-      red = create_car(color: "red", purchased_on: "1973-10-04", doors: 4, make_id: subaru.id)
-      blue = create_car(color: "blue", purchased_on: "2012-01-24", doors: 2, make_id: toyota.id)
+      red = create_car(:color => "red", :purchased_on => "1973-10-04", :doors => 4, :make_id => subaru.id)
+      blue = create_car(:color => "blue", :purchased_on => "2012-01-24", :doors => 2, :make_id => toyota.id)
 
       get '/cars', {}, {'Accept' => 'application/json'}
 
@@ -55,6 +59,43 @@ describe "Make Cars API" do
 
       expect(response.code.to_i).to eq 200
       expect(JSON.parse(response.body)).to eq(expected_response)
+    end
+  end
+
+  describe 'GET /cars/id' do
+
+    it "returns the car for a specific car" do
+      subaru = create_make(:name => "something")
+
+      red = create_car(:color => "red", :purchased_on => "1973-10-04", :doors => 4, :make_id => subaru.id)
+
+      expected_response = {
+        "_links" => {
+          "self" => {
+            "href" => car_path(red)
+          },
+          "make" => {
+            "href" => make_path(red.make)
+          }
+        },
+        "id" => red.id,
+        "color" => "red",
+        "doors" => 4,
+        "purchased_on" => "1973-10-04"
+      }
+
+      get "/cars/#{red.id}", {}, {'Accept' => 'application/json'}
+
+      expect(response.code.to_i).to eq 200
+      expect(JSON.parse(response.body)).to eq(expected_response)
+    end
+
+
+    it "returns a 404 if the specific car isn't found" do
+      get "/cars/1000", {}, {'Accept' => 'application/json'}
+
+      expect(response.code.to_i).to eq 404
+      expect(JSON.parse(response.body)).to eq({})
     end
   end
 end
